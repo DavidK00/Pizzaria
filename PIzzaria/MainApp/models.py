8b824e43fa647171efd1ea2f0379a178eadf8987
DavidK00 <Dav3dkoo@gmail.com>
2022-05-06 17:24:52 -0500

    5/6/2022

--- PIzzaria/MainApp/models.py
+++ PIzzaria/MainApp/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Pizza(models.Model):
+    pizza_name = models.CharField(max_length = 30)
+
+    def __str__(self):
+        return self.pizza_name
+
+class Toppings(models.Model):
+    pizza = models.ForeignKey(Pizza, on_delete=models.CASCADE)
+    topping = models.CharField(max_length = 30)
+
+    class Meta:
+        verbose_name_plural = 'toppings'
+
+    def __str__(self):
+        return self.topping
+
+
+
--- PIzzaria/MainApp/views.py
+++ PIzzaria/MainApp/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+from .models import Pizza, Toppings
+
 
 # Create your views here.
+def index(request):
+    return render(request, 'MainApp/index.html')
+
+def pizza(request):
+    pizzas = Pizza.objects.all()
+    template = loader.get_template('MainApp/list.html')
+    context = {'pizzas':pizzas}
+    return HttpResponse(template.render(context, request))
+
+def indiv_pizza(request, pizza_id):
+    indiv_pizza = Pizza.objects.get(id = pizza_id)
+    toppings = Toppings.toppings_set()
+    template = loader.get_template('MainApp/pizza.html')
+    context = {'indiv_pizza':indiv_pizza, "toppings": toppings}
+    return HttpResponse(template.render(context, request))
+
+
+
+
+
+
+
+    
+
+    
